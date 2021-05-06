515e14a3724d92749024d43ff937a0666fd6664c
Peter Roto <peterdroto@gmail.com>
2021-05-05 22:22:26 -0500

    got color changing based on time of day done, change a bunch of HTML elem attributes to make saving notes easier

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,100 +2,65 @@
 var curDateTime = moment();
 $("#currentDay").text(curDateTime.format("dddd, MMM Do YYYY"));
 
-var renderClock = function() {
-    $("#currentTime").text(moment().format("h:mm:ss a"));
-}
+//Current hour for conditional formatting
+var currentHour = curDateTime.format('HH');
 
-//Set HTML table rows as an object array in JS
-$('#timeTable tr').each(function(row, tr) {
-    tableData = tableData +
-        $(tr).find('th:eq(0)').text() + ' '
-});
-
+//Create a new array filled with table rows and their corresponding value.
 var tableData = new Array();
-$('#timeTable tr').each(function(row, tr) {
+$('#timeTable tr').each(function(row, th) {
     tableData[row] = {
-        SyncValue: $(tr).find('.timeGrid').data('value'),
+        Hour: $(th).find('.timeGrid').data('value'),
     }
 });
 tableData.shift();
 
-//Color coding Blocks
-var blockTime = [{
-        SyncValue: "",
-        Name: "9AM",
-        Time: "9:00:00 am",
-    },
-    {
-        SyncValue: "",
-        Name: "10AM",
-        Time: "10:00:00 am",
-    },
-    {
-        SyncValue: "",
-        Name: "11AM",
-        Time: "11:00:00 am",
-    },
-    {
-        SyncValue: "",
-        Name: "12PM",
-        Time: "12:00:00 pm",
-    },
-    {
-        SyncValue: "",
-        Name: "1PM",
-        Time: "1:00:00 pm",
-    },
-    {
-        SyncValue: "",
-        Name: "2PM",
-        Time: "2:00:00 pm",
-    },
-    {
-        SyncValue: "",
-        Name: "3PM",
-        Time: "3:00:00 pm",
-    },
-    {
-        SyncValue: "",
-        Name: "4PM",
-        Time: "4:00:00 pm",
-    },
-    {
-        SyncValue: "",
-        Name: "5PM",
-        Time: "5:00:00 pm",
-    },
-]
-
-
-
-$(function() {
-    for (i = 0; i < blockTime.length; i++) {
-        blockTime[i].SyncValue = tableData[i].SyncValue
-        tableData[i].Time = blockTime[i].Time
+//Create function to check if the block is in the past,present of future
+var stateCheck = function() {
+    for (i = 0; i < tableData.length; i++) {
+        $("tbody tr").each(function() {
+            if (currentHour > $(this).data('value')) {
+                $(this).addClass('past');
+                $(this).removeClass('future');
+                $(this).removeClass('present');
+            } else if (currentHour < $(this).data('value')) {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('future');
+                $(this).addClass('present');
+            }
+        });
     };
-});
+};
 
+//Run stateCheck every 30 seconds.
+window.onload = function() {
+    stateCheck();
+
+    // setTimeout(stateCheck, 1000 * 30);
+}
+
+var renderClock = function() {
+    $("#currentTime").text(moment().format("h:mm:ss a"));
+}
 
 //Render clock every second
 $(function() {
     setInterval(renderClock, 1000);
 });
 
+var savedNotes = []
 
-//Create Each function for all arrays
-Array.prototype.each = function(callback) {
-    for (var i = 0; i < this.length; i++) {
-        callback(this[i]);
-    }
+function getEventText() {
+    var eventText = $(this).prev().children('textarea').val();
+    var eventID = $(this).prev().children('textarea').attr('id');
+    console.log('ID=', eventID);
+    console.log('eventText=', eventText);
+
+
+
 }
 
-console.log(timeTable.each())
-
-/* SEE IF NEEDED 
-    
-
-
-
-*/
+$('.saveBtn').on('click', getEventText);
